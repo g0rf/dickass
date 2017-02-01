3871613950f51deceacccaf2ded27f3197fd226b
Mike Gedelman <mikegedelman@gmail.com>
2017-01-31 23:34:53 -0600

    move pepe

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,13 +1,50 @@
-import RainbowText from 'objects/RainbowText';
+// import RainbowText from 'objects/RainbowText';
 
 class GameState extends Phaser.State {
 
-	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		text.anchor.set(0.5);
+	init() {
+		const game = this.game;
+		this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 	}
 
+	preload() {
+		this.game.load.image('pepe', '/pepe.jpg');
+	}
+
+	create() {
+		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
+		this.pepe = this.game.add.sprite(center.x, center.y, 'pepe');
+		this.game.physics.enable(this.pepe, Phaser.Physics.ARCADE);
+	}
+
+	update() {
+		const v = 300; // movement speed
+
+		if (!this.pepe.body) {
+			return;
+		}
+
+		if (this.upKey.isDown) {
+			this.pepe.body.velocity.y = -v;
+		} else if (this.downKey.isDown) {
+			this.pepe.body.velocity.y = v;
+		} else {
+				this.pepe.body.velocity.y = 0;
+		}
+
+
+		if (this.leftKey.isDown) {
+			this.pepe.body.velocity.x = -v;
+		} else if (this.rightKey.isDown) {
+			this.pepe.body.velocity.x = v;
+		}
+		else {
+			this.pepe.body.velocity.x = 0;
+		}
+	}
 }
 
 export default GameState;
