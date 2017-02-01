d85a3613c6e3c24695308637a3e11dc65db4b24d
CODE-HOG <user@lab205-04.watkins.edu>
2017-02-01 11:50:00 -0600

    add john movement

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ mainState.prototype = {
 
 	preload: function() { // load pepe
 		game.load.image('pepe', 'assets/pepe.jpg');
+        game.load.image('john', 'assets/john.png')
 	},
 
 	create: function() { // add pepe as a sprite, with physics
 		this.pepe = this.game.add.sprite(game.world.centerX, game.world.centerY, 'pepe');
 		game.physics.enable(this.pepe, Phaser.Physics.ARCADE);
+        
+        this.john = this.game.add.sprite(game.world.centerX, game.world.centerY, 'john');
+		game.physics.enable(this.john, Phaser.Physics.ARCADE);
 	},
 
 	update: function() { // move pepe on keypress, stop him otherwise
@@ -36,6 +40,25 @@ mainState.prototype = {
 		}
 		else {
 			this.pepe.body.velocity.x = 0;
+		}
+        
+        //mr john
+        if (this.upKey.isDown) {
+			this.john.body.velocity.y = -v;
+		} else if (this.downKey.isDown) {
+			this.john.body.velocity.y = v;
+		} else {
+				this.john.body.velocity.y = 0;
+		}
+
+
+		if (this.leftKey.isDown) {
+			this.john.body.velocity.x = -v;
+		} else if (this.rightKey.isDown) {
+			this.john.body.velocity.x = v;
+		}
+		else {
+			this.john.body.velocity.x = 0;
 		}
 	}
 }
