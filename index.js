af1c8e0e5c851f0bd745baf9ef31e1eab3dbc90e
Mike Gedelman <mikegedelman@gmail.com>
2017-02-01 08:50:19 -0600

    simplify

---
+++ index.js
@@ -0,0 +1,61 @@
+
+class GameState extends Phaser.State {
+
+	init() {
+		const game = this.game;
+		this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+	}
+
+	preload() {
+		this.game.load.image('pepe', 'assets/pepe.jpg');
+	}
+
+	create() {
+		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
+		this.pepe = this.game.add.sprite(center.x, center.y, 'pepe');
+		this.game.physics.enable(this.pepe, Phaser.Physics.ARCADE);
+	}
+
+	update() {
+		const v = 300; // movement speed
+
+		if (!this.pepe.body) {
+			return;
+		}
+
+		if (this.upKey.isDown) {
+			this.pepe.body.velocity.y = -v;
+		} else if (this.downKey.isDown) {
+			this.pepe.body.velocity.y = v;
+		} else {
+				this.pepe.body.velocity.y = 0;
+		}
+
+
+		if (this.leftKey.isDown) {
+			this.pepe.body.velocity.x = -v;
+		} else if (this.rightKey.isDown) {
+			this.pepe.body.velocity.x = v;
+		}
+		else {
+			this.pepe.body.velocity.x = 0;
+		}
+	}
+}
+
+
+
+class Game extends Phaser.Game {
+
+	constructor() {
+		super(500, 500, Phaser.AUTO, 'content', null);
+		this.state.add('GameState', GameState, false);
+		this.state.start('GameState');
+	}
+
+}
+
+new Game();
