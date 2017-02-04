326324b1b448a79e9ccdd50fd92593399df0a86d
g0rf <mike.gedelman@gmail.com>
2017-02-03 21:16:31 -0600

    big suprise

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var platforms;
 
 var sounds = {
-	music:new Audio("assets/sounds/soulbossanova.mp3"),
-	jumpsound:new Audio("assets/sounds/badpoosy.mp3")
+	music: new Audio("assets/sounds/soulbossanova.mp3"),
+	jumpsound: new Audio("assets/sounds/badpoosy.mp3")
 };
 
 var mainState = function(game){};
@@ -10,27 +10,22 @@ var titleState = function(game){};
 
 titleState.prototype = {
 	init: function() {
-		
 		upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-		
 	},
-	
+
 	preload: function () {
 		game.load.image('sky', 'assets/sky.png');
-	
 	},
-	
+
 	create: function () {
-		
 		game.add.sprite(0, 0, 'sky');
-		var titleScreen = game.add.text(230, 80, 'The Adventures of Dickass', {font:'25px Arial', fill: '#ffffff'});
-		
-		var howToStart = game.add.text(titleScreen.width, 120, 'Press up to start party', {font:'15px Arial', fill: '#ffffff'});
+		var titleScreen = game.add.text(230, 80, 'The Adventures of Dickass', { font: '25px Arial', fill: '#ffffff '});
+
+		var howToStart = game.add.text(titleScreen.width, 120, 'Press up to start party', { font: '15px Arial', fill: '#ffffff' });
 		cursors = game.input.keyboard.createCursorKeys();
 	},
-	
+
 	update: function () {
-	
 		if (cursors.up.isDown) {
 			startGame();
 		}
@@ -38,7 +33,6 @@ titleState.prototype = {
 }
 
 mainState.prototype = {
-
 	init: function() { // register keyboard inputs
 		this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
 		this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
@@ -47,117 +41,97 @@ mainState.prototype = {
 
 	},
 
-	preload: function() { // load pepe
-		
+	preload: function() { // load assets
 		game.load.image('pepe', 'assets/pepe.jpg');
-        game.load.image('john', 'assets/john.png');
-		game.load.image('dickass', 'assets/dickass-pixel.png');
+		game.load.image('john', 'assets/john.png');
+		game.load.spritesheet('dickass', 'assets/dickass-spritesheet.png', 100, 100);
 		game.load.image('sky', 'assets/sky.png');
 		game.load.image('ground', 'assets/platform-pixel.png');
-		
-
 	},
-	
-	
-	create: function() { // add pepe as a sprite, with physics
-	
+
+	create: function() {
+
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		
+
 		game.add.sprite(0, 0, 'sky');
-		
+
+		// Ground stuff
 		platforms = game.add.group();
-		
 		platforms.enableBody = true;
-	
+
 		var ground = platforms.create(0, game.world.height - 32, 'ground');
-	
 		ground.body.immovable = true;
 
 		var ground2 = platforms.create(396, game.world.height - 32, 'ground');
 		ground2.body.immovable = true;
 		var ground3 = platforms.create(396*2, game.world.height - 32, 'ground');
 		ground3.body.immovable = true;
-		
-		
+
+		// Hovering platforms stuff
 		var ledge = platforms.create(450, 400, 'ground'); //right ledge
-	
 		ledge.body.immovable = true;
-		
-		ledge = platforms.create(-100, 250, 'ground'); //left ledge
-	
+		var ledge2 = platforms.create(-100, 250, 'ground'); //left ledge
 		ledge.body.immovable = true;
-		
+
 		this.pepe = this.game.add.sprite(game.world.centerX, game.world.centerY - 200, 'pepe');
-		game.physics.enable(this.pepe, Phaser.Physics.ARCADE);
-        
-        this.john = this.game.add.sprite(game.world.centerX-150, game.world.centerY - 200, 'john');
-		game.physics.enable(this.john, Phaser.Physics.ARCADE);
-		
+
+		this.john = this.game.add.sprite(game.world.centerX-150, game.world.centerY - 200, 'john');
+
 		this.dickass = this.game.add.sprite(game.world.centerX, game.world.centerY, 'dickass');
+		this.dickass.faceLeft = function() {
+			this.anchor.setTo(.5,.5);
+			this.scale.x = -1;
+		}
+		this.dickass.faceRight = function() {
+			this.anchor.setTo(.5,.5);
+			this.scale.x = 1;
+		}
+
 		game.physics.enable(this.dickass, Phaser.Physics.ARCADE);
-		
-		this.dickass.scale.setTo(.8,.8);
-		
+
+		// this.dickass.scale.setTo(.8,.8);
+
 		//mike doesn't like bounce that ass bounce bounce that ass
 		//this.dickass.body.bounce.y = 0.5;
 		this.dickass.body.gravity.y = 1000;
 		this.dickass.body.collideWorldBounds = true;
-		
+
 		cursors = game.input.keyboard.createCursorKeys();
 	},
 
-	update: function() { // move pepe on keypress, stop him otherwise
+	update: function() {
 		var v = 300; // movement speed
-
-		
 		var hitPlatform = game.physics.arcade.collide(this.dickass, platforms);
-		
-			 //  Reset the players velocity (movement)
+
+		//  Reset the players velocity (movement)
 		this.dickass.body.velocity.x = 0;
 
-		if (cursors.left.isDown)
-		{
+		if (cursors.left.isDown) {
 			//  Move to the left
 			this.dickass.body.velocity.x = -150;
-
-			this.dickass.animations.play('left');
-		}
-		else if (cursors.right.isDown)
-		{
+			this.dickass.faceLeft();
+			this.dickass.frame = 1; // frame 1 is ROCKET SKATES LMFAO
+		} else if (cursors.right.isDown) {
 			//  Move to the right
 			this.dickass.body.velocity.x = 150;
-
-			this.dickass.animations.play('right');
-		}
-		else
-		{
+			// this.dickass.animations.play('right');
+			this.dickass.frame = 1;
+			this.dickass.faceRight();
+		} else {
 			//  Stand still
 			this.dickass.animations.stop();
-
-			this.dickass.frame = 4;
+			this.dickass.frame = 0;
 		}
 
 		//  Allow the player to jump if they are touching the ground.
-		if (cursors.up.isDown && this.dickass.body.touching.down && hitPlatform)
-		{
+		if (cursors.up.isDown && this.dickass.body.touching.down && hitPlatform) {
 			this.dickass.body.velocity.y = -600;
 			sounds.jumpsound.play();
-		
 		}
 	}
 }
 
-
-
-
-// 500x500 screen, engine is Phaser.AUTO, id of the element to put this in
-
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', null);
-/*game.state.add('mainState', mainState, false);
-game.state.start('mainState');
-sounds.music.play();
-sounds.music.volume = .75;
-sounds.jumpsound.volume = 0.5;*/
 
 game.state.add('titleState', titleState, false);
 game.state.start('titleState');
