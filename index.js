4826f1a57a2b13e6fc306d11b891056cfe12c856
CODE-HOG <cweber@watkins.edu>
2017-02-10 20:41:34 -0600

    big piggy

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ var sounds = {
 	jumpsound: new Audio("assets/sounds/badpoosy.mp3")
 };
 
+
 var mainState = function(game){};
 var titleState = function(game){};
+var sfw = false;
+var totalEnemies = 0;
+var enemiesAlive = 0;
+
 
 titleState.prototype = {
 	init: function() {
 		upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
 	},
 
 	preload: function () {
@@ -19,16 +25,26 @@ titleState.prototype = {
 
 	create: function () {
 		game.add.sprite(0, 0, 'sky');
-		var titleScreen = game.add.text(230, 80, 'The Adventures of Dickass', { font: '25px Arial', fill: '#ffffff '});
+		var titleScreen = game.add.text(230, 80, 'The Adventures of Dickass', { font: '25px Arial', fill: '#ffffff'});
 
-		var howToStart = game.add.text(titleScreen.width, 120, 'Press up to start party', { font: '15px Arial', fill: '#ffffff' });
+		var howToStart = game.add.text(titleScreen.width, 120, 'Press up to start party', { font: '15px Arial', fill: '#ffffff'});
+        var howToStart = game.add.text(titleScreen.width, 150, 'Press down if u normal', { font: '15px Arial', fill: '#ffffff'});
+
+        
 		cursors = game.input.keyboard.createCursorKeys();
 	},
 
 	update: function () {
 		if (cursors.up.isDown) {
-			startGame();
+            startGame();
 		}
+        
+        if (cursors.down.isDown) {
+            console.log(true);
+            sfw = true;
+            startGame();
+            
+        }
 	}
 }
 
@@ -53,19 +69,13 @@ function Dickass(game) {
 	this.bullets = game.add.group();
 	this.bullets.enableBody = true;
 	this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-	this.bullets.createMultiple(30, 'john');
-	this.bullets.fireRate = 500;
-	this.bullets.bulletSpeed = 600;
-
-	this.weapon = game.add.weapon(50, 'john');
-	this.weapon.enableBody = true;
-
-	this.bulletAngleVariance = 10;
-	this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-	this.weapon.bulletAngleOffset = 90;
-	this.weapon.fireRate = 500;
-	this.weapon.bulletSpeed = 600;
-
+	this.bullets.createMultiple(10, 'john');
+    this.bullets.setAll('outOfBoundsKill', true);
+    this.bullets.setAll('checkWorldBounds', true);
+    this.bulletTime = 0;
+    this.fireRate = 50;
+    
+    
 	var fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
 	// Add our overlay spritesheet as a child, so it always moves with dickass
@@ -88,18 +98,27 @@ function Dickass(game) {
 
 	this.fire = function() {
 		var multiplier = 1;
-		if (sprite.scale.x !== 1) { // if scale.x is not 1, then he's facing left
+        if (sprite.scale.x !== 1) { // if scale.x is not 1, then he's facing left
 				multiplier = -1;
 		}
-
-		//  Grab the first bullet we can from the pool
-		var bullet = this.bullets.getFirstExists(false);
-
-		if (bullet) {
-				bullet.reset(sprite.x, sprite.y);
-				bullet.body.velocity.x = 600 * multiplier;
-				// bulletTime = game.time.now + 200;
-		}
+        //  Grab the first bullet we can from the pool
+        
+        if (game.time.now > this.bulletTime && this.bullets.countDead() > 0) {
+            this.bulletTime = game.time.now + this.fireRate;
+            
+            var bullet = this.bullets.getFirstExists(false);
+        
+            bullet.reset(sprite.x, sprite.y);
+            bullet.body.velocity.x = 600 * multiplier;
+        }
+        
+        
+        /*if (bullet) {
+                bullet.reset(sprite.x, sprite.y);
+                bullet.body.velocity.y = -600;
+                
+               //this.bulletTime = game.time.now + 2000;
+        }*/
 	};
 
 	// Start off facing right. Call it here on creation, to avoid a weird jump thing that happens
@@ -107,32 +126,43 @@ function Dickass(game) {
 
 	// Main update function. Called in gameState update().
 	this.update = function() {
+        
 		//  Reset the players velocity (movement)
 		sprite.body.velocity.x = 0;
-
+        sprite.body.velocity.y = 0;
 		// Movement Stuff
 		if (cursors.left.isDown) {
 			//  Move to the left
 			sprite.body.velocity.x = -150;
 			faceLeft();
-			rocketOverlaySprite.frame = 1; // frame 1 is ROCKET SKATES LMFAO
+			//rocketOverlaySprite.frame = 1; // frame 1 is ROCKET SKATES LMFAO
 
 		} else if (cursors.right.isDown) {
 			//  Move to the right
 			sprite.body.velocity.x = 150;
-			rocketOverlaySprite.frame = 1;
+			//rocketOverlaySprite.frame = 1;
 			faceRight();
 		} else {
 			//  Stand still
-			rocketOverlaySprite.frame = 0;
+			//rocketOverlaySprite.frame = 0;
 		}
-
+        
+        if(cursors.up.isDown) {
+            // Move up
+            sprite.body.velocity.y = -150;
+            rocketOverlaySprite.frame = 1; // frame 1 is ROCKET SKATES LMFAO
+        } else if (cursors.down.isDown) {
+            // Move down
+            sprite.body.velocity.y = 150;
+        } else {
+            sprite.body.velocity.y = 0;
+            rocketOverlaySprite.frame = 0;
+        }
 		//  Allow the player to jump if they are standing on top of anything
-		if (cursors.up.isDown && sprite.body.blocked.down) {
+		/*if (cursors.up.isDown && sprite.body.blocked.down) {
 			sprite.body.velocity.y = -600;
-			sounds.jumpsound.play();
-		}
-
+			sounds.jumpsound.play();*/
+		
 		// Weapon switch stuff
 		if (game.input.keyboard.isDown(Phaser.Keyboard.ONE)) {
 			// unarmed overlay
@@ -147,13 +177,29 @@ function Dickass(game) {
 			sprite.frame = 1;
 			overlaySprite.frame = 2;
 		}
-
+        
 		if (fireButton.isDown)	{
-			this.fire();
-		}
-	}
+            this.fire();
+        }
+    },
+        
+    this.render = function() {
+        game.debug.spriteInfo(sprite, 32, 32);
+        game.debug.text(this.bullets, 32, 16);
+    }
 }
 
+function Pepe(game) {
+    
+    //game.physics.arcade.enable(group);
+
+    this.pepes = game.add.group();
+    this.pepes.enableBody = true;
+		for (var i = 0; i < 5; i++)  {
+			this.pepe = this.pepes.create(game.world.centerX, i * 150, 'pepe');
+			this.pepe.body.gravity.x = -150;    
+            }
+}
 mainState.prototype = {
 
 	init: function() { // register keyboard inputs
@@ -168,13 +214,31 @@ mainState.prototype = {
 		game.load.tilemap('level1', 'assets/platform.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('gameTiles', 'assets/platform-pixel.png');
 
+        
+//        game.load.image('dickass', 'assets/john.png');
+//        game.load.image('dickass', 'assets/dickass/dickass-spritesheet-dark-border.png', 100, 100);
+      
+        if (sfw == true) {
+            game.load.image('dickass', 'assets/piggy.png');
+            game.load.spritesheet('dickassOverlay', 'assets/smaller-overlay.png', 50, 50);
+            game.load.spritesheet('dickassRocketOverlay', 'assets/dickass-rocket-overlay.png', 100, 100);
+        }   else {
+            game.load.spritesheet('dickass', 'assets/dickass-spritesheet-dark-border.png', 100, 100);
+            game.load.spritesheet('dickassOverlay', 'assets/smaller-overlay.png', 50, 50);
+            game.load.spritesheet('dickassRocketOverlay', 'assets/dickass-rocket-overlay.png', 100, 100);
+            game.load.image('arnold', 'assets/arnold.png');
+        }
+    
+       
 		game.load.image('pepe', 'assets/pepe.jpg');
 		game.load.image('john', 'assets/john.png');
-		game.load.spritesheet('dickass', 'assets/dickass-spritesheet-dark-border.png', 100, 100);
+        
+        
+        // game.load.spritesheet('dickass', 'assets/dickass-spritesheet-dark-border.png', 100, 100);
 		// game.load.spritesheet('dickassOverlay', 'assets/dickass-overlay.png', 100, 100);
-		game.load.spritesheet('dickassOverlay', 'assets/smaller-overlay.png', 50, 50);
-		game.load.spritesheet('dickassRocketOverlay', 'assets/dickass-rocket-overlay.png', 100, 100);
-		game.load.image('arnold', 'assets/arnold.png');
+		//game.load.spritesheet('dickassOverlay', 'assets/smaller-overlay.png', 50, 50);
+		//game.load.spritesheet('dickassRocketOverlay', 'assets/dickass-rocket-overlay.png', 100, 100);
+		//game.load.image('arnold', 'assets/arnold.png');
 		game.load.image('sky', 'assets/sky.png');
 		game.load.image('ground', 'assets/platform-pixel.png');
 	},
@@ -192,20 +256,21 @@ mainState.prototype = {
 		this.map.setCollisionBetween(1, 100000, true, 'collision');
 		this.backgroundlayer.resizeWorld();
 
-		this.pepes = game.add.group();
-		this.pepes.enableBody = true;
-		for (var i = 0; i < 5; i++)  {
-			this.pepe = this.pepes.create(game.world.centerX, i*150, 'pepe');
-			this.pepe.body.gravity.x = -6;
-		}
-
-		this.arnold = game.add.sprite(game.world.centerX - 200, 400, 'arnold');
-		game.physics.arcade.enable(this.arnold);
-		this.arnold.body.gravity.y = 1000;
+		
+		
+        
+		
+        if(sfw == false) {
+            console.log('arnold added');
+            this.arnold = game.add.sprite(game.world.centerX - 200, 400, 'arnold');
+            game.physics.arcade.enable(this.arnold);
+            this.arnold.body.gravity.y = 1000;
+        }
+      
 
 		// Create dickass
 		this.dickass = new Dickass(game);
-
+        this.pepes = new Pepe(game);
 		//changed from game.input to this.input. not sure if that does anything
 		cursors = this.input.keyboard.createCursorKeys();
 	},
@@ -215,13 +280,19 @@ mainState.prototype = {
 
 		// collision stuff
 		game.physics.arcade.collide(this.dickass.sprite, this.collisionLayer);
-    game.physics.arcade.collide(this.dickass.sprite, this.pepes);
-    game.physics.arcade.overlap(this.dickass.bullets, this.pepes, killPepe, null, this);
+        game.physics.arcade.collide(this.dickass.sprite, this.pepes.pepes);
+        game.physics.arcade.overlap(this.dickass.bullets, this.pepes.pepes, killPepe, null, this);
+        game.physics.arcade.overlap(this.dickass.sprite, this.pepes.pepes, killDickass, null, this);
 		game.physics.arcade.collide(this.arnold, this.collisionLayer);
-    game.physics.arcade.collide(this.dickass.sprite, this.arnold);
+        game.physics.arcade.collide(this.dickass.sprite, this.arnold);
 
 		this.dickass.update();
-	},
+        //this.pepe.update();
+	},   
+    
+    render: function() {
+        this.dickass.render();
+    }
 }
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', null);
@@ -233,7 +304,7 @@ function startGame() {
 	game.state.add('mainState', mainState, false);
 	game.state.start('mainState');
 	sounds.music.volume = .75;
-	sounds.music.play();
+	//sounds.music.play();
 	sounds.jumpsound.volume = 0.5
 }
 
@@ -241,3 +312,9 @@ function killPepe(bullets, pepe) {
     pepe.kill();
     bullets.kill();
 }
+
+function killDickass(dickass, pepe) {
+    dickass.kill();
+    pepe.kill();
+}
+
